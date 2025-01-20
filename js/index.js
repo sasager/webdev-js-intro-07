80a9791abd08f3ff0de0418705dd3d02902fc4bb
Sierra <sierra.ar.sager@gmail.com>
2025-01-19 19:38:01 -0500

    completed experiment

--- js/index.js
+++ js/index.js
@@ -2,3 +2,29 @@
 // that are not declared
 "use strict"
 
+function add(a, b) {
+    return a + b;
+}
+
+let sum = add(24, 32);
+console.log(sum);
+
+function calculateVolume(length, width, height) {
+    return length * width * height;
+}
+
+let volume = calculateVolume(2, 4, 6);
+console.log("Volume of object:", volume);
+
+function greetUser(name) {
+    console.log("Hello, " + name + "! Thank you for visiting our website.");
+}
+
+greetUser("Kitty Cat");
+
+function sayWhat(name, message = "What?") {
+    console.log(name + ", " + message);
+}
+
+sayWhat("Karen");
+sayWhat("Pikachu", "Huh?");
